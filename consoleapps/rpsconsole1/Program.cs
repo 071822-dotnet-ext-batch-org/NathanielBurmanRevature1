5d466bc8565e4c36c2f97618ecee703b0f3b13d0
Nathaniel Burman <nathaniel132@revature.net>
2022-07-21 14:06:56 -0400

    new test app

---
+++ consoleapps/rpsconsole1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace rpsconsole1
+{
+    class Program
+    {
+        
+        public static string msg(string s)
+        {
+            string sMsg = "hello world";
+            return sMsg;
+        }
+
+        public static string msg(int i)
+        {
+            string sMsg;
+            sMsg = "11011100";
+            sMsg = "10010011";
+            return sMsg;
+        }
+        
+        static void Main(string[] args)
+        {
+            // write a string
+            System.Console.WriteLine("Hello World!");
+
+            // write the result of a method
+            Console.WriteLine(msg('s'));
+
+            // write the result of a method
+            Console.WriteLine(msg(1));
+
+            // a different set of parameters
+            // write an integer
+            Console.WriteLine(43);
+
+            int test;
+            int test2;
+            test = 11111111;
+            test = 11110233123000000;
+
+        }
+
+        
+    }
+}
