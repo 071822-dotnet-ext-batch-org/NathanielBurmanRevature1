9fa15242671c4e05048e3a0e60541af63fe1f83e
Nathaniel Burman <nathaniel132@revature.net>
2022-08-22 11:24:21 -0400

    0822

---
+++ project/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace IndiabixConsoleApplication
+{ 
+    class index
+    {
+        protected int count;
+        public index()
+        {
+            count = 0;
+        }
+    }
+    class index1: index
+    {
+        public void increment()
+        {
+            count = count +1;
+        }
+    }
+    class MyProgram
+    {
+        static void Main(string[] args)
+        {
+            index1 i = new index1(); 
+            i.increment(); 
+        }
+    }
+}
